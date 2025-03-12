f0d4f69b84504a7d28f0fd6f6c64b82bd26d0247
AprilWinds <1031360756@qq.com>
2025-03-12 19:09:01 +0800

    feat(ip): ip分配器实现

--- internal/network/ls.go
+++ internal/network/ls.go
@@ -1,7 +1,6 @@
 package network
 
 import (
-	"encoding/json"
 	"fmt"
 	"mini_docker/internal/util"
 	"os"
@@ -18,14 +17,11 @@ func LS() {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', tabwriter.Debug)
 	fmt.Fprintln(w, "ID\tNAME\tSUBNET\tGATEWAY\tDRIVER")
 	for _, file := range files {
-		var n Network
-		f, err := os.Open(filepath.Join(stroageRootDir, file.Name()))
+		n, err := getNetwork(file.Name())
 		if err != nil {
 			os.RemoveAll(filepath.Join(stroageRootDir, file.Name()))
 			continue
 		}
-		defer f.Close()
-		json.NewDecoder(f).Decode(&n)
 		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", file.Name(), n.Name, n.Subnet, n.Gateway, n.Driver)
 	}
 	w.Flush()
--- internal/network/types.go
+++ internal/network/types.go
@@ -7,7 +7,11 @@ type Network struct {
 	Subnet  string `json:"subnet"`
 	Driver  string `json:"driver"`
 	Gateway string `json:"gateway"`
+	IPM     *IPMgr `json:"ip_mgr"`
 }
 
 type IPMgr struct {
+	StartIP uint32              `json:"start_ip"`
+	EndIP   uint32              `json:"end_ip"`
+	Used    map[uint32]struct{} `json:"used"`
 }
