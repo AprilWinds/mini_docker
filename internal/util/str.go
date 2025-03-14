b18f14a99ebc5969155ac2044bfb6767dea731bf
AprilWinds <1031360756@qq.com>
2025-03-14 10:42:42 +0800

    feat(veth): 更改veth命名

--- internal/util/str.go
+++ internal/util/str.go
@@ -2,21 +2,12 @@ package util
 
 import (
 	"math/rand"
-	"strings"
 )
 
 func GetRandomStr() string {
 	bytes := make([]byte, 6)
 	for i := range bytes {
-		bytes[i] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"[rand.Intn(62)]
+		bytes[i] = "0123456789abcdefghijklmnopqrstuvwxyz"[rand.Intn(36)]
 	}
 	return string(bytes)
-}
-
-func ReverseStr(s string) string {
-	builder := strings.Builder{}
-	for i := len(s) - 1; i >= 0; i-- {
-		builder.WriteByte(s[i])
-	}
-	return builder.String()
 }
